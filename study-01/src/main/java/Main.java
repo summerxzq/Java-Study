deec85c0ae5abcc2ca8ec11eb41cf02b220d962f
xiazhiqiang <726942552@qq.com>
2021-07-22 17:32:54 +0800

    hot-fix commit3

--- study-01/src/main/java/Main.java
+++ study-01/src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
         System.out.println("hello git");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix");
     }
     }
 
